453a2366f930196e5d3602da677ab434cd48f572
Wolfgang Bumiller <w.bumiller@proxmox.com>
2015-12-09 15:40:00 +0100

    backup: vma: remove async queue

--- blockdev.c
+++ blockdev.c
@@ -3126,6 +3126,11 @@ static void pvebackup_cancel(void *opaque)
         error_setg(&backup_state.error, "backup cancelled");
     }
 
+    if (backup_state.vmaw) {
+        /* make sure vma writer does not block anymore */
+        vma_writer_set_error(backup_state.vmaw, "backup cancelled");
+    }
+
     /* drain all i/o (awake jobs waiting for aio) */
     bdrv_drain_all();
 
@@ -3138,6 +3143,7 @@ static void pvebackup_cancel(void *opaque)
             if (job) {
                 if (!di->completed) {
                      block_job_cancel_sync(job);
+                     bdrv_drain_all(); /* drain all i/o (awake jobs waiting for aio) */
                 }
             }
         }
